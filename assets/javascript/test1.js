04467f130f9f6449dced2354cb1a0c62eaa8fb98
annette.beatty@gmail.com <annette.beatty@gmail.com>
2018-07-05 14:58:27 -0700

    Incorporated John's zipcode logic

--- assets/javascript/test1.js
+++ assets/javascript/test1.js
@@ -1,11 +1,14 @@
-// Doing bulk groupon pull, but the processing serially in Yelp
+// Incorporating John's zip logic and resets after showing results
 
 $(document).ready(function () {
     var saveit = [];
-    var numToPull = 10;
-    var grponPull = 20;
+    var numToPull = 10; // Number of deals we'll show
+    var grponPull = 50; // Number we're pulling from Groupon
     var grpOffset = 0;
     var processing = 0;
+    var grpnLocStr = "";
+    var gresponse = {};
+    var inProgress = false;
 
     // Set up the CORS server link
     $.ajaxPrefilter(function (options) {
@@ -14,15 +17,80 @@ $(document).ready(function () {
         }
     });
 
-    startTheShow();
+    // This button just uses standard Groupon query which uses current location based
+    //  on IP when no other location is entered.
 
-    function startTheShow()
+    $("#curLoc").on("click", function (event) 
+    {
+        event.preventDefault();
+
+        if (inProgress)
+            return;
+
+        grpnLocStr="";
+        startTheShow(grpnLocStr);
+    });
+
+    // This segment gets zipcode, and and then processes it into lat and long for Groupon 
+    // (who doesn't take zip codes :( ))
+    // First get the value in the button
+
+    $("#getLoc").on("click", function (event) 
+    {
+        event.preventDefault();
+
+        if (inProgress)
+            return;
+  
+        var zip = $("#zipCode").val().trim();
+        console.log("getLoc: entered zip: ", zip);
+  
+        //  make sure it is a valid zip code (or good enough)
+        if ((parseInt(zip)) > 1000 && (parseInt(zip)) < 99951) 
+        {
+          var zurl = "https://www.zipcodeapi.com/rest/NnhKKXxjVlQfKghOtdW83dPHEUYPi8K41sMduv0tzFaf0M5GitUpmwyv1S3pA9xn/info.json/"+zip+"/degrees";
+  
+          $.ajax(zurl)
+              .then(function (zipOut) 
+              {
+                  console.log("getLoc: GPS: ", zipOut);
+                  grpnLocStr="&lat="+zipOut.lat+"&lng="+zipOut.lng;
+                  console.log("getLoc: Groupon Location String: ", grpnLocStr);
+                  console.log("getLoc: city name: ",zipOut.city);
+                  $("#city").text(zipOut.city+", "+zipOut.state);
+  
+                  startTheShow(grpnLocStr);
+              });
+        } 
+        else 
+        {
+          $("#city").text("Please enter a valid US zip code")
+        };
+    });
+
+    function resetAll ()
+    {   
+        grpOffset = 0;
+        processing = 0;
+        grpnLocStr = "";
+        gresponse = {};
+        inProgress = false;
+        $("#resultsDiv").empty();
+    }
+
+    function startTheShow(grpnLocStr)
     {
         console.log("startTheShow: Entering");
         console.log("startTheShow: NumToPull = ", numToPull, "processing = ", processing);
 
+        resetAll();
+
+        // Set the in progress flag so they can't do another selection until we're done
+        inProgress = true;
+
+
         // Kick off the search
-        groupOnSearch(groupOnComplete);
+        groupOnSearch(grpnLocStr, groupOnComplete);
     }
 
     // Groupon Data is collected
@@ -47,12 +115,18 @@ $(document).ready(function () {
             getYelpID(grpOnArray, processMultiple);
 
         }
+        else  
+        {
+            inProgress = false;
+
+        }
+
     }
 
     // This function goes out to Groupon and grabs deals
-    function groupOnSearch(groupOnComplete)
+    function groupOnSearch(grpnLocStr,groupOnComplete)
     {
-        var url = "https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&lat=32.853431&lng=-117.182872&filters=category:food-and-drink&limit=" + grponPull + "&offset=" + grpOffset;
+        var url = "https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0" + grpnLocStr + "&filters=category:food-and-drink&limit=" + grponPull + "&offset=" + grpOffset;
 
         $.ajax(url)
         .then(function (response) 
@@ -140,7 +214,7 @@ $(document).ready(function () {
         var data = grpOnArray[processing];
         var Name = encodeURIComponent(data.name.trim());
         var Street = encodeURIComponent(data.streetAddress.trim());
-        var City = data.city;
+        var City = encodeURIComponent(data.city.trim());
         var State = data.state;
 
         var term = "name=" + Name;
@@ -149,16 +223,38 @@ $(document).ready(function () {
 
         // $.ajax(url, { headers: { Authorization: 'Bearer IOnOmcVyQA7g8bfItyRwB1JFyfXeJh0kXRqdwyKUjuxOP2LmvLLth68IN84LwKiAUSgtQN5Bikqdnm70id-_Sj_0U5vTewXNl7ycBkUayA45WB-ozhQ2VEq7-6AuW3Yx' }})
         $.ajax(url, { headers: { Authorization: 'Bearer s8fyDTIEAcaKIhVHE-YXji0_G6gyCKWLxbwwL5Hg1PQW-Eu_ErKZ-xeV0_xRqQ0VtEV7XpS540SpNB9q4aQkcW-fp43IhgOgfh0fHP_d8YdNVHCqqxgMCBDQ8_U6W3Yx' } })
-            .then(function (response) {
+            .then(function (response)
+            {
                 console.log("getYelpID: Yelp response", response);
                 console.log("getYelpID: Yelp array", response.businesses.length);
-                data.yelpID = response.businesses[0].id;
-                console.log("getYelpID: Yelp ID: ", data.yelpID);
-                console.log("getYelpID: grpOnArray : ", data);
+                if (response.businesses.length > 0)
+                {
+                    data.yelpID = response.businesses[0].id;
+                    console.log("getYelpID: Yelp ID: ", data.yelpID);
+                    console.log("getYelpID: grpOnArray : ", data);
 
-                // Got the yelp ID, now get the yelp rich data
-                getYelpData(grpOnArray, processMultiple);
-        });
+                    // Got the yelp ID, now get the yelp rich data
+                    getYelpData(grpOnArray, processMultiple);
+                }
+                else
+                {
+                    console.log("getYelpID: Can't find this business - ", data.name);
+                    // Take it out of the Groupon Array and process the next element
+                    grpOnArray.splice (processing, 1);
+                    processMultiple(grpOnArray);
+                }
+            },
+
+            // Error processing here
+            function()
+            {
+                console.log("getYelpID: Yelp returned error - ", data.name);
+
+                // Take it out of the Groupon Array and process the next element
+                grpOnArray.splice (processing, 1);
+                processMultiple(grpOnArray);
+            }
+        );
     }
 
     function getYelpData(grpOnArray, callback)
@@ -182,7 +278,10 @@ $(document).ready(function () {
                 data.streetAddress = response.location.address1;
                 data.rating = response.rating;
                 data.price = response.price;
-                data.open = response.hours[0].is_open_now;
+                if (response.hours == null)
+                    data.open = "N/A";
+                else
+                    data.open = response.hours[0].is_open_now;
                 data.review_count = response.review_count;
                 data.categories = response.categories;
                 
@@ -203,7 +302,7 @@ function showit (displayObject) {
 
     var divIt = "<a href='" + displayObject.dealUrl + "' target='_blank'>" + displayObject.deal + "</a>";
 
-    console.log("DivIt = ", divIt);
+    console.log("showit: DivIt = ", divIt);
 
     // *** need to work in here *** //
 
